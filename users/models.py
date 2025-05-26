cd97ab707376f0d5bc851c9dc19f6c563496548d
Vadim Shlykov <shlikov.vadim@gmail.com>
2025-05-27 01:53:43 +0300

    added save method to save image in correct dimensions if it is larger than required size

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from PIL import Image
 
 
 def user_avatar_path(instance, filename):
@@ -12,6 +13,15 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField(upload_to=user_avatar_path, default='avatars/default.png', blank=True)
     bio = models.TextField(blank=True, null=True)
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
     
     def __str__(self):
         return f'Профиль {self.user.username}'
